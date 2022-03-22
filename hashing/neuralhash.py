5d2520f7e59e90ea4fad879aa34c6250faf53d26
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-03-22 13:45:49 +0100

    Add neural architectures

--- hashing/neuralhash.py
+++ hashing/neuralhash.py
@@ -19,7 +19,8 @@ from torchvision.models import inception_v3
 import scipy.spatial.distance as distance
 from hashing.imagehash import ImageHash
 from hashing.general_hash import Algorithm
-from hashing.SimCLR import resnet_wider 
+from hashing.SimCLRv1 import resnet_wider as SIMv1
+from hashing.SimCLRv2 import resnet as SIMv2
 
 path = os.path.abspath(__file__)
 current_folder = os.path.dirname(path)
@@ -219,48 +220,114 @@ def load_inception_v3(device='cuda'):
     return inception
 
 
-
-def load_simclr_v1_resnet50_2x(device='cuda'):
+def load_simclr_v1(width):
     """
-    Load the simclr v1 ResNet2x model.
+    Load the simclr v1 ResNet50 model with the given width.
 
     Parameters
     ----------
-    device : str, optional
-        Device on which to load the model. The default is 'cuda'.
+    width : int
+        Width multiplier.
 
     Returns
     -------
-    simclr : PyTorch model
-        The simclr v1 ResNet2x model.
+    load : function
+        A loader function for the simclr v1 ResNet50 1x model.
 
     """
     
-    assert (device=='cpu' or device=='cuda')
+    checkpoint_file = current_folder + f'/SimCLRv1/Pretrained/resnet50-{width}x.pth'
     
-    # Load the model 
-    simclr = resnet_wider.resnet50x2()
-    checkpoint = torch.load(current_folder + '/SimCLR/resnet50-2x.pth')
-    simclr.load_state_dict(checkpoint['state_dict'])
-    simclr.fc = nn.Identity()
-    simclr.eval()
-    simclr.to(torch.device(device))
+    def load(device='cuda'):
+        
+        assert (device=='cpu' or device=='cuda')
+        
+        # Load the model 
+        simclr = SIMv1.get_resnet(width=width)
+        checkpoint = torch.load(checkpoint_file)
+        simclr.load_state_dict(checkpoint['state_dict'])
+        simclr.fc = nn.Identity()
+        simclr.eval()
+        simclr.to(torch.device(device))
     
-    return simclr
+        return simclr
+    
+    return load
+
+
+
+def load_simclr_v2(depth, width, selective_kernel=True):
+    """
+    Load the simclr v2 ResNet model with given depth and width.
+
+    Parameters
+    ----------
+    depth : int
+        Depth of the ResNet model
+    width : int
+        Width multiplier.
+    selective_kernel : Boolean
+        Whether to use a selective kernel.
+
+    Returns
+    -------
+    load : function
+        a Loader function for the simclr v2 ResNet model with given depth and width.
+
+    """
+    
+    if selective_kernel:
+        checkpoint_file = current_folder + f'/SimCLRv2/Pretrained/r{depth}_{width}x_sk1_ema.pth'
+    else:
+        checkpoint_file = current_folder + f'/SimCLRv2/Pretrained/r{depth}_{width}x_ema.pth'
+    
+    def load(device='cuda'):
+        
+        assert (device=='cpu' or device=='cuda')
+        
+        # Load the model 
+        sk_ratio = 0.0625 if selective_kernel else 0
+        simclr, _ = SIMv2.get_resnet(depth=depth, width_multiplier=width, sk_ratio=sk_ratio)
+        checkpoint = torch.load(checkpoint_file)
+        simclr.load_state_dict(checkpoint['resnet'])
+        simclr.eval()
+        simclr.to(torch.device(device))
+    
+        return simclr
+    
+    
+    return load
+
+
 
 
 # Mapping from string to actual algorithms
 NEURAL_MODEL_LOADER = {
     'Inception v3': load_inception_v3,
-    'SimCLR v1 ResNet50 2x': load_simclr_v1_resnet50_2x
+    'SimCLR v1 ResNet50 1x': load_simclr_v1(width=1),
+    'SimCLR v1 ResNet50 2x': load_simclr_v1(width=2),
+    'SimCLR v1 ResNet50 4x': load_simclr_v1(width=4),
+    'SimCLR v2 ResNet152 3x': load_simclr_v2(depth=152, width=3, selective_kernel=True),
     }
 
 
 # Mapping from string to feature size output of networks
 NEURAL_MODEL_FEATURES_SIZE = {
     'Inception v3': 2048,
-    'SimCLR v1 ResNet50 2x': 4096
+    'SimCLR v1 ResNet50 1x': 2048,
+    'SimCLR v1 ResNet50 2x': 4096,
+    'SimCLR v1 ResNet50 4x': 8192,
+    'SimCLR v2 ResNet152 3x': 6144,
     }
+
+
+# Transforms for all SimCLR models
+SIMCLR_TRANSFORMS = T.Compose([
+    T.Resize(256, interpolation=T.InterpolationMode.LANCZOS),
+    T.CenterCrop(224),
+    T.ToTensor()
+    ])
+
 
 # Mapping from string to pre-processing transforms of networks
 NEURAL_MODEL_TRANSFORMS = {
@@ -270,11 +337,13 @@ NEURAL_MODEL_TRANSFORMS = {
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ]),
     
-    'SimCLR v1 ResNet50 2x': T.Compose([
-        T.Resize(256, interpolation=T.InterpolationMode.LANCZOS),
-        T.CenterCrop(224),
-        T.ToTensor()
-        ])
+    'SimCLR v1 ResNet50 1x': SIMCLR_TRANSFORMS,
+    
+    'SimCLR v1 ResNet50 2x': SIMCLR_TRANSFORMS,
+    
+    'SimCLR v1 ResNet50 4x': SIMCLR_TRANSFORMS,
+    
+    'SimCLR v2 ResNet152 3x': SIMCLR_TRANSFORMS,
     }
 
                 
