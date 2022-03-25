fb0948f597728a7fdf128821007e51fd6147ed5a
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-03-25 15:06:09 +0100

    test

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ algos = [
                             device='cuda', distance='Jensen-Shannon'),
     ]
 
-thresholds = [np.linspace(0, 0.4, 10), np.linspace(0, 0.1, 10), np.linspace(0, 0.1, 10), 
+thresholds = [np.linspace(0, 0.4, 10), np.linspace(4, 12, 10), np.linspace(100, 250, 10), 
               np.linspace(0, 0.8, 10)]
     
 positive_dataset = hashing.create_dataset(path_experimental, existing_attacks=True)
