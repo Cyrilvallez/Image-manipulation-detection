b4d3d762ee79844c851fccd549c3d56438bf59a5
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-04-01 13:51:27 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=%x-%j.err
 #SBATCH --time=1-00:00:00
 #SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=10240
+#SBATCH --mem-per-gpu=40G
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:2
 #SBATCH --chdir=/cluster/raid/home/cyril.vallez/Project
