c3e4fbf7f95869b919df3c1e088d1b8a34c233d1
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-04-05 15:38:44 +0200

    Update test_JS.py

--- test_JS.py
+++ test_JS.py
@@ -18,8 +18,10 @@ import hashing
 import hashing.neuralhash as nh
 from torch.utils.data import Dataset, IterableDataset, DataLoader
 
-algo = hashing.NeuralAlgorithm('SimCLR v2 ResNet50 2x', raw_features=True, batch_size=512,
+algo_ori = hashing.NeuralAlgorithm('SimCLR v2 ResNet50 2x', raw_features=True, batch_size=512,
                         device='cuda', distance='Jensen-Shannon')
+algo_test = hashing.NeuralAlgorithm('SimCLR v2 ResNet50 2x', raw_features=True, batch_size=512,
+                        device='cuda', distance='Test_torch')
 
 path_database = 'Datasets/ILSVRC2012_img_val/Experimental/'
 path_experimental = 'Datasets/ILSVRC2012_img_val/Experimental/'
@@ -28,21 +30,26 @@ path_database = [path_database + file for file in os.listdir(path_database)][0:1
 path_experimental = [Image.open(path_experimental + file) for file in os.listdir(path_experimental)[0:1]]
 
 
-database_test = algo.create_database(path_database, {})
-database_original = super(type(algo), algo).create_database(path_database, {})
+database_test = algo_test.create_database(path_database, {})
+database_original = super(type(algo_ori), algo_ori).create_database(path_database, {})
 
 
-img = algo.preprocess(path_experimental)
-algo.load_model()
-fingerprint = algo.process_batch(img)
-algo.kill_model()
+img_test = algo_test.preprocess(path_experimental)
+algo_test.load_model()
+fingerprint_test = algo_test.process_batch(img_test)
+algo_test.kill_model()
+
+img_ori = algo_ori.preprocess(path_experimental)
+algo_ori.load_model()
+fingerprint_ori = algo_ori.process_batch(img_ori)
+algo_ori.kill_model()
 
 t0 = time.time()
-distances = fingerprint[0].compute_distances_torch(database_test)
+distances = fingerprint_test[0].compute_distances_torch(database_test)
 dt_new = time.time() - t0
 
 t0 = time.time()
-distances2 = fingerprint[0].compute_distances(database_original)
+distances2 = fingerprint_ori[0].compute_distances(database_original)
 dt_old = time.time() - t0
 
 print(f'Same : {np.allclose(distances, distances2)}')
