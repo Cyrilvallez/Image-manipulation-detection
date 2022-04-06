0056f0a5cef2f5dc61baa55347dfee6f8a31c2a8
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-04-06 09:32:35 +0200

    test

--- test_JS.py
+++ test_JS.py
@@ -27,7 +27,7 @@ algo_test = hashing.NeuralAlgorithm('SimCLR v2 ResNet50 2x', raw_features=True, 
 path_database = 'Datasets/ILSVRC2012_img_val/Experimental/'
 path_experimental = 'Datasets/ILSVRC2012_img_val/Experimental/'
 
-path_database = [path_database + file for file in os.listdir(path_database)][0:1000]
+path_database = [path_database + file for file in os.listdir(path_database)][0:10000]
 path_experimental = [Image.open(path_experimental + file) for file in os.listdir(path_experimental)[0:1]]
 
 
