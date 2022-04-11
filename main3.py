8cc710f26005ba7dd48520ca817da9667948bd2b
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-04-11 16:36:41 +0200

    update

--- main3.py
+++ main3.py
@@ -24,41 +24,21 @@ path_experimental = 'Datasets/BSDS500/Experimental_attacks/'
 path_control = 'Datasets/BSDS500/Control_attacks/'
 
 algos = [
-    hashing.FeatureAlgorithm('SIFT', batch_size=1000, n_features=30, cutoff=1),
-    hashing.FeatureAlgorithm('SIFT', batch_size=1000, n_features=100, cutoff=1),
-    hashing.FeatureAlgorithm('SIFT', batch_size=1000, n_features=200, cutoff=1),
-    hashing.FeatureAlgorithm('SIFT', batch_size=1000, n_features=300, cutoff=1),
-    hashing.FeatureAlgorithm('ORB', batch_size=1000, n_features=30, cutoff=1),
-    hashing.FeatureAlgorithm('ORB', batch_size=1000, n_features=100, cutoff=1),
-    hashing.FeatureAlgorithm('ORB', batch_size=1000, n_features=200, cutoff=1),
-    hashing.FeatureAlgorithm('ORB', batch_size=1000, n_features=300, cutoff=1),
-    hashing.FeatureAlgorithm('FAST + DAISY', batch_size=1000, n_features=30, cutoff=1),
-    hashing.FeatureAlgorithm('FAST + DAISY', batch_size=1000, n_features=100, cutoff=1),
-    hashing.FeatureAlgorithm('FAST + DAISY', batch_size=1000, n_features=200, cutoff=1),
-    hashing.FeatureAlgorithm('FAST + DAISY', batch_size=1000, n_features=300, cutoff=1),
-    hashing.FeatureAlgorithm('FAST + LATCH', batch_size=1000, n_features=30, cutoff=1),
-    hashing.FeatureAlgorithm('FAST + LATCH', batch_size=1000, n_features=100, cutoff=1),
-    hashing.FeatureAlgorithm('FAST + LATCH', batch_size=1000, n_features=200, cutoff=1),
-    hashing.FeatureAlgorithm('FAST + LATCH', batch_size=1000, n_features=300, cutoff=1),
+    hashing.NeuralAlgorithm('Inception v3', raw_features=True, batch_size=512,
+                            device='cuda', distance='cosine'),
+    hashing.NeuralAlgorithm('Inception v3', raw_features=True, batch_size=512,
+                            device='cuda', distance='L2'),
+    hashing.NeuralAlgorithm('Inception v3', raw_features=True, batch_size=512,
+                            device='cuda', distance='L1'),
+    hashing.NeuralAlgorithm('Inception v3', raw_features=True, batch_size=512,
+                            device='cuda', distance='Jensen-Shannon'),
     ]
 
 thresholds = [
-    np.linspace(0, 300, 20),
-    np.linspace(0, 300, 20),
-    np.linspace(0, 300, 20),
-    np.linspace(0, 300, 20),
-    np.linspace(0, 0.3, 20),
-    np.linspace(0, 0.3, 20),
-    np.linspace(0, 0.3, 20),
-    np.linspace(0, 0.3, 20),
     np.linspace(0, 0.4, 20),
-    np.linspace(0, 0.4, 20),
-    np.linspace(0, 0.4, 20),
-    np.linspace(0, 0.4, 20),
-    np.linspace(0.1, 0.4, 20),
-    np.linspace(0.1, 0.4, 20),
-    np.linspace(0.1, 0.4, 20),
-    np.linspace(0.1, 0.4, 20),
+    np.linspace(6, 30, 20),
+    np.linspace(200, 800, 20),
+    np.linspace(0.15, 0.65, 20),
     ]
     
     
@@ -67,6 +47,6 @@ negative_dataset = hashing.create_dataset(path_control, existing_attacks=True)
 
 
 digest = hashing.total_hashing(algos, thresholds, path_database, positive_dataset,
-                               negative_dataset, general_batch_size=1000)
+                               negative_dataset, general_batch_size=512)
 
 utils.save_digest(digest, save_folder)
