df54693d01bae2afe257fe3e1d91aec2e932aead
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-04-27 17:42:03 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=32000
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:v100:1
 #SBATCH --chdir=/cluster/raid/home/cyril.vallez/Project
 
 # Verify working directory
